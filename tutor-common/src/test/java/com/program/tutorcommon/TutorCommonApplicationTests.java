a02c7eea289a2462361300c6bace3c4710659156
Li <453984016@qq.com>
2018-01-31 17:35:55 +0800

    aa

---
+++ tutor-common/src/test/java/com/program/tutorcommon/TutorCommonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.program.tutorcommon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TutorCommonApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
