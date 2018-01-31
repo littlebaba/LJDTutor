62e0b71eec934aa50ded9b6a303642350972e88f
Li <453984016@qq.com>
2018-01-31 23:42:26 +0800

    entity

--- tutor-common/src/main/java/com/program/tutorcommon/entity/Student.java
+++ tutor-common/src/main/java/com/program/tutorcommon/entity/Student.java
@@ -1,15 +1,20 @@
 package com.program.tutorcommon.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.program.tutorcommon.utils.Constants;
+
 import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
 
 /**
  * @author Li on 2018/1/31.
- *
+ * <p>
  * 学生
  */
 @Entity
 @Table(name = "totur_student")
-public class Student {
+public class Student implements Serializable {
 
     @Id
     @GeneratedValue
@@ -18,7 +23,6 @@ public class Student {
     //联系人信息
     @Column(nullable = false)
     private String contactName;
-
 
     private String fixedTel;
 
@@ -49,5 +53,141 @@ public class Student {
     private String pay;
 
 
+    @Column(nullable = false)
+    @JsonFormat(pattern = Constants.DATE_FORMAT, timezone = "GMT+8")
+    private Date initDate;
+
+    public Date getInitDate() {
+        return initDate;
+    }
+
+    public void setInitDate(Date initDate) {
+        this.initDate = initDate;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+
+    public String getFixedTel() {
+        return fixedTel;
+    }
+
+    public void setFixedTel(String fixedTel) {
+        this.fixedTel = fixedTel;
+    }
+
+    public String getMobilePhone() {
+        return mobilePhone;
+    }
+
+    public void setMobilePhone(String mobilePhone) {
+        this.mobilePhone = mobilePhone;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getAreaDes() {
+        return areaDes;
+    }
+
+    public void setAreaDes(String areaDes) {
+        this.areaDes = areaDes;
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public String getClassTime() {
+        return classTime;
+    }
+
+    public void setClassTime(String classTime) {
+        this.classTime = classTime;
+    }
+
+    public String getStudentDes() {
+        return studentDes;
+    }
+
+    public void setStudentDes(String studentDes) {
+        this.studentDes = studentDes;
+    }
+
+    public Integer getReqTeaSex() {
+        return reqTeaSex;
+    }
+
+    public void setReqTeaSex(Integer reqTeaSex) {
+        this.reqTeaSex = reqTeaSex;
+    }
+
+    public String getReqTeaDes() {
+        return reqTeaDes;
+    }
+
+    public void setReqTeaDes(String reqTeaDes) {
+        this.reqTeaDes = reqTeaDes;
+    }
+
+    public String getPay() {
+        return pay;
+    }
+
+    public void setPay(String pay) {
+        this.pay = pay;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", contactName='" + contactName + '\'' +
+                ", fixedTel='" + fixedTel + '\'' +
+                ", mobilePhone='" + mobilePhone + '\'' +
+                ", area='" + area + '\'' +
+                ", areaDes='" + areaDes + '\'' +
+                ", sex=" + sex +
+                ", grade='" + grade + '\'' +
+                ", classTime='" + classTime + '\'' +
+                ", studentDes='" + studentDes + '\'' +
+                ", reqTeaSex=" + reqTeaSex +
+                ", reqTeaDes='" + reqTeaDes + '\'' +
+                ", pay='" + pay + '\'' +
+                ", initDate=" + initDate +
+                '}';
+    }
 
 }
--- tutor-common/src/main/java/com/program/tutorcommon/entity/Teacher.java
+++ tutor-common/src/main/java/com/program/tutorcommon/entity/Teacher.java
@@ -1,7 +1,249 @@
 package com.program.tutorcommon.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.program.tutorcommon.utils.Constants;
+
+import javax.persistence.*;
+import java.util.Date;
+
 /**
  * @author Li on 2018/1/31.
  */
+@Entity
+@Table(name = "totur_teacher")
 public class Teacher {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    //注册信息
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    @JsonIgnore
+    private String password;
+
+    //教员基本信息
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer sex;//0:男，1：女
+
+    @Column(nullable = false)
+    @JsonFormat(pattern = Constants.DATE_FORMAT, timezone = "GMT+8")
+    private Date birthday;
+
+    @Column(nullable = false)
+    @JsonFormat(pattern = Constants.DATE_FORMAT, timezone = "GMT+8")
+    private Date initDate;
+
+    private String birthPlace;
+
+
+    private String education;
+
+    @Column(nullable = false)
+    private String IdNumber;
+
+    private String profession;
+
+    private String graduatedSchool;
+
+    //联系方式
+    private String livePlace;
+
+    private String workPlace;
+
+    private String tel;
+    @Column(nullable = false)
+    private String mobile;
+
+    private String QQ;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String contactAddr;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getBirthPlace() {
+        return birthPlace;
+    }
+
+    public void setBirthPlace(String birthPlace) {
+        this.birthPlace = birthPlace;
+    }
+
+    public String getEducation() {
+        return education;
+    }
+
+    public void setEducation(String education) {
+        this.education = education;
+    }
+
+    public String getIdNumber() {
+        return IdNumber;
+    }
+
+    public void setIdNumber(String idNumber) {
+        IdNumber = idNumber;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
+    public String getGraduatedSchool() {
+        return graduatedSchool;
+    }
+
+    public void setGraduatedSchool(String graduatedSchool) {
+        this.graduatedSchool = graduatedSchool;
+    }
+
+    public String getLivePlace() {
+        return livePlace;
+    }
+
+    public void setLivePlace(String livePlace) {
+        this.livePlace = livePlace;
+    }
+
+    public String getWorkPlace() {
+        return workPlace;
+    }
+
+    public void setWorkPlace(String workPlace) {
+        this.workPlace = workPlace;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getQQ() {
+        return QQ;
+    }
+
+    public void setQQ(String QQ) {
+        this.QQ = QQ;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContactAddr() {
+        return contactAddr;
+    }
+
+    public void setContactAddr(String contactAddr) {
+        this.contactAddr = contactAddr;
+    }
+
+    public Date getInitDate() {
+        return initDate;
+    }
+
+    public void setInitDate(Date initDate) {
+        this.initDate = initDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", name='" + name + '\'' +
+                ", sex=" + sex +
+                ", birthday=" + birthday +
+                ", initDate=" + initDate +
+                ", birthPlace='" + birthPlace + '\'' +
+                ", education='" + education + '\'' +
+                ", IdNumber='" + IdNumber + '\'' +
+                ", profession='" + profession + '\'' +
+                ", graduatedSchool='" + graduatedSchool + '\'' +
+                ", livePlace='" + livePlace + '\'' +
+                ", workPlace='" + workPlace + '\'' +
+                ", tel='" + tel + '\'' +
+                ", mobile='" + mobile + '\'' +
+                ", QQ='" + QQ + '\'' +
+                ", email='" + email + '\'' +
+                ", contactAddr='" + contactAddr + '\'' +
+                '}';
+    }
 }
