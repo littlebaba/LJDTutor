19ea6bd380a3a6aebf9f23462e2cb57f048497a7
Li <453984016@qq.com>
2018-01-31 17:34:24 +0800

    a

---
+++ tutor-common/src/main/java/com/program/tutorcommon/TutorCommonApplication.java
@@ -0,0 +1,12 @@
+package com.program.tutorcommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorCommonApplication.class, args);
+	}
+}
